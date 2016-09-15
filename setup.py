f05b2f368e98e90e0eb5998e26f0eb7031d30904
Charles Leifer <charles.leifer@propylon.pri>
2016-09-15 08:52:44 -0500

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='sqlite-web',
-    version='0.1.5',
+    version='0.1.6',
     description='Web-based SQLite database browser.',
     long_description=long_description,
     author='Charles Leifer',
