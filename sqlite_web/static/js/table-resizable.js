9348cdeb2b8385a466d4d9ecf708e8c79d7ab861
patarapolw <patarapolw@gmail.com>
2018-08-01 23:17:04 +0700

    Hide sidebar when hovering over the table.

--- sqlite_web/static/js/table-resizable.js
+++ sqlite_web/static/js/table-resizable.js
@@ -1,7 +1,16 @@
 $(function() {
-  $("th").each(function(){
-    $(this).css('width', ($(this).width() + 10) + 'px');
-  });
+  $('.table-container').hover(function(){
+      $('#sidebar').hide();
+      $('#content').removeClass('col-9').addClass('col');
+    }, function(){
+      $('#sidebar').show();
+      $('#content').addClass('col-9').removeClass('col');
+    });
+
+    $("th").each(function(){
+      $(this).css('width', ($(this).width() + 10) + 'px');
+    });
+
   $("table").colResizable({
     liveDrag: true,
     resizeMode: 'overflow'
