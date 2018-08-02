ac275b8e8554678dddb6acd81cb288541e49c07b
patarapolw <patarapolw@gmail.com>
2018-08-02 14:27:10 +0700

    Move full window handler to content

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='sqlite-web',
-    version='0.3.3.1-dev2',
+    version='0.3.3.1-dev3',
     description='Web-based SQLite database browser.',
     long_description=long_description,
     author='Charles Leifer',
--- sqlite_web/static/js/table-resizable.js
+++ sqlite_web/static/js/table-resizable.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.table-container').hover(function(){
+  $('#content').hover(function(){
       $('#sidebar').hide();
       $('#content').removeClass('col-9').addClass('col');
     }, function(){
@@ -7,9 +7,9 @@ $(function() {
       $('#content').addClass('col-9').removeClass('col');
     });
 
-    $("th").each(function(){
-      $(this).css('width', ($(this).width() + 10) + 'px');
-    });
+  $("th").each(function(){
+    $(this).css('width', ($(this).width() + 10) + 'px');
+  });
 
   $("table").colResizable({
     liveDrag: true,
