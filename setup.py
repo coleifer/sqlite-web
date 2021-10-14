ee63de31aa77510f1d9ef7bd412eafe3c54539c2
Charles Leifer <coleifer@gmail.com>
2021-10-14 07:57:03 -0500

    0.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='sqlite-web',
-    version='0.3.9',
+    version='0.4.0',
     description='Web-based SQLite database browser.',
     long_description='Web-based SQLite database browser.',
     author='Charles Leifer',
